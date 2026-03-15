00cd8d20588707e5f0654fb0215675301ae35a91
harsh <harsh@example.com>
2026-03-15 20:20:46 +0530

    google  redirecting  url

--- app/config.py
+++ app/config.py
@@ -9,6 +9,6 @@ REDIS_URL = os.getenv("REDIS_URL")
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 
-GOOGLE_REDIRECT_URI = "http://localhost:8000/auth/gmail/callback"
+GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI", "http://localhost:8000/auth/gmail/callback")
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
